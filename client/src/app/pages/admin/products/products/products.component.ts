35e44769ef430b2cb9c74414b45628cd9120fb1c
nika <go.tiit.ogle@gmail.com>
2024-01-28 17:07:40 +0300

    Btn

--- client/src/app/pages/admin/products/products/products.component.ts
+++ client/src/app/pages/admin/products/products/products.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {SuccessBtnComponent} from "../../../../components/buttons/success-btn/success-btn.component";
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SuccessBtnComponent],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
@@ -12,7 +13,7 @@ export class ProductsComponent {
 
   // Переменная для отображения
   isSidePanelVisible: boolean = false;
-
+  btnBoolean =  false
 
   openSidePanel(){
     this.isSidePanelVisible = true;
@@ -20,6 +21,9 @@ export class ProductsComponent {
 
   closeSidePanel(){
     this.isSidePanelVisible = false;
+  }
+  getSuccessBtnData(event: any){
+    this.btnBoolean = event
   }
 
 }
